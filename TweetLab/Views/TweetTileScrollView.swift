43d2f9ac6537c18b52eb0dfb12d50a59cd928aad
Matt Ao <mattyayoh@gmail.com>
2019-02-20 00:58:14 -0800

    scrollView receives notifications when tweets will and did load, progress spinner kicks off, and tweet tiles get reloaded

--- TweetLab/Views/TweetTileScrollView.swift
+++ TweetLab/Views/TweetTileScrollView.swift
@@ -13,29 +13,50 @@ class TweetTileScrollView: UIView {
    var tweetManager: TweetManager
    let scrollView:UIScrollView
    let tileStack:UIStackView
+   let progressSpinner:UIActivityIndicatorView
    
    required init?(coder aDecoder: NSCoder) {
-      tweetManager = TweetManager.init(gameID: "fake", tweets: [])
-      tweetManager.refreshTweets()
+      tweetManager = TweetManager.init()
       scrollView = UIScrollView()
       tileStack = UIStackView()
+      progressSpinner = UIActivityIndicatorView(style: .gray)
       super.init(coder: aDecoder)
-      scrollView.frame = self.bounds
-      scrollView.isPagingEnabled = true
-      tileStack.frame = self.bounds
-      let scrollWidth = CGFloat(tweetManager.tweets.count) * scrollView.frame.width
-      scrollView.contentSize = CGSize.init(width: scrollWidth, height: scrollView.frame.height)
-      scrollView.showsVerticalScrollIndicator = false
-      scrollView.showsHorizontalScrollIndicator = false
-      self.addSubview(scrollView)
+      progressSpinner.frame = self.bounds
+      progressSpinner.hidesWhenStopped = true
+      self.addSubview(progressSpinner)
       
+      tileStack.frame = self.bounds
       tileStack.axis = .horizontal
       tileStack.distribution = .fillEqually
       
+      scrollView.frame = self.bounds
+      scrollView.isPagingEnabled = true
+      scrollView.showsVerticalScrollIndicator = false
+      scrollView.showsHorizontalScrollIndicator = false
+      self.addSubview(scrollView)
       scrollView.addSubview(tileStack)
-      tileStack.frame = CGRect.init(x: 0, y: 0, width: scrollWidth, height: scrollView.frame.height)
       tileStack.translatesAutoresizingMaskIntoConstraints = true
       tileStack.autoresizingMask = [ .flexibleWidth, .flexibleHeight ]
+
+      NotificationCenter.default.addObserver(self, selector: #selector(tweetsWillLoad(notification:)), name: .tweetsWillLoad, object: nil)
+      NotificationCenter.default.addObserver(self, selector: #selector(tweetsDidLoad(notification:)), name: .tweetsDidLoad, object: nil)
+   }
+   
+   @objc func tweetsWillLoad(notification:Notification) {
+      scrollView.isHidden = true
+      progressSpinner.startAnimating()
+   }
+   
+   @objc func tweetsDidLoad(notification:Notification) {
+      let scrollWidth = CGFloat(tweetManager.tweets.count) * scrollView.frame.width
+      scrollView.contentSize = CGSize.init(width: scrollWidth, height: scrollView.frame.height)
+      tileStack.frame = CGRect.init(x: 0, y: 0, width: scrollWidth, height: scrollView.frame.height)
+      scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+      scrollView.isHidden = false
+      
+      for tile in tileStack.subviews {
+         tile.removeFromSuperview()
+      }
       
       var i:CGFloat = 0
       for tweet in tweetManager.tweets {
@@ -43,5 +64,7 @@ class TweetTileScrollView: UIView {
          tileStack.addArrangedSubview(newTile)
          i += 1
       }
+      
+      progressSpinner.stopAnimating()
    }
 }
